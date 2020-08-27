cdf4df09f34828388765cd0c58546834480abfc6
zy <1142867174@qq.com>
2020-08-27 12:46:09 +0800

    第一次添加文件

---
+++ src/components 组件/common 其它项目也可以用的/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+.tab-bar {
+  display: flex;
+  height: 49px;
+  background-color: #f9f9f9;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components 组件/common 其它项目也可以用的/tabbar/TabBarItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="flag">
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon"></slot>
+    </div>
+    <div :class="{active:flag}">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+  },
+
+  computed: {
+    flag() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
+.active {
+  color: red;
+}
+</style>
